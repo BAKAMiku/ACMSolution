3194a9b67f994b4e404e826523db8f3e042624ff
BakaMiku <bakamiku@qq.com>
2017-12-24 12:40:41 +0800

    fix the format of 45\46

--- 1046.cpp
+++ 1046.cpp
@@ -1,1 +1,10 @@
-#include<stdio.h>int main(){    long long f[56];    f[0]=0;f[1]=1;f[2]=2;f[3]=3;    int i,n;    for(i=4;i<56;i++)        f[i]=f[i-1]+f[i-3];    while(scanf("%d",&n)!=EOF&&n!=0)        printf("%lld\n",f[n]);}
+#include<stdio.h>
+int main(){
+	long long f[56];
+	f[0]=0;f[1]=1;f[2]=2;f[3]=3;
+	int i,n;
+	for(i=4;i<56;i++)
+		f[i]=f[i-1]+f[i-3];
+	while(scanf("%d",&n)!=EOF&&n!=0)
+		printf("%lld\n",f[n] );
+}
